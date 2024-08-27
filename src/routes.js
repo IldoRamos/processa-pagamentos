16f06caacce82c51700ef31c9fc4b8339def3915
IldoRamos <ildoramosvieira@gmail.com>
2024-08-27 08:42:15 -0300

    ajuste na rota de integração dos dos serviços

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
+const pagamentoController = require('./controllers/pagamentoController');
 const router = express.Router();
 
-
+router.post('/pedido', pagamentoController.processaPagamentoController);
 module.exports = router;
